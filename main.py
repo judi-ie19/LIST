2cf8f13c70bfa237677334897649c35da90ec8b5
jud-ie19 <owuorakinyijudith@gmail.com>
2022-06-09 22:37:56 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# . Write a Python program to sum all the items in a list. 
+#input
+# list1=[1,2,3,4,5]
+# list1=1+2+3+4+5
+# output
+# 15
+
+from subprocess import list2cmdline
+
+
+def sum(numbers):
+    sum=0
+    for i in numbers:
+     sum+=i
+    print (sum)
+
+sum([1,2,3,4,7,8])
+
+# Write a python program that prints out a new list of all 
+# elements in the list by multiplied by 2 
+# input
+# list2=[1,5,7,6,8,10]
+# output
+# list2=([1,5,7,6,8,10])*2
+
+def new_list(numbers):
+   list=[]
+   for w in numbers:
+       w*2
+       list.append(w*2)
+       print ()
+      
+
+  
+       
+        
+    
+    
+    
+
+
+    
+
+    
+
+
+   
+
+
+
+
+        
+  
+ 
+
+
+
